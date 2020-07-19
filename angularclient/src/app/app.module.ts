946ebf6f23addb08210ba41a9bf17736ee25dc6f
jskipper <jesper.skipper@live.com>
2020-07-19 16:46:32 +0200

    more angular added

---
+++ angularclient/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
