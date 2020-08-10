63766c286a039712e1335fb0247f13ea3f774a23
jskipper <jesper.skipper@live.com>
2020-08-10 10:21:18 +0200

    continued with the basics

--- client/src/app/shared/car/car.service.ts
+++ client/src/app/shared/car/car.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class CarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/cool-cars');
+  }
 }
--- springokta/src/main/java/com/skipper/springokta/CoolCarController.java
+++ springokta/src/main/java/com/skipper/springokta/CoolCarController.java
@@ -1,5 +1,6 @@
 package com.skipper.springokta;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
