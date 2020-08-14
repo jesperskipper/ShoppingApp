65cd1868010fb761c1ffe6946681d5060e1ac74b
jskipper <jesper.skipper@live.com>
2020-08-14 20:04:57 +0200

    code for editing cars is added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,8 @@ import {MatListModule} from "@angular/material/list";
 import { CarEditComponent } from './car-edit/car-edit.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import {FormsModule} from "@angular/forms";
     MatCardModule,
     MatListModule,
     MatFormFieldModule,
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
